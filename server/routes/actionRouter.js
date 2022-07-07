43d605cd83ce7a74adea45d7de8fb338b4600e88
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-07-07 14:32:36 +0300

    fix: remove methods

--- server/routes/actionRouter.js
+++ server/routes/actionRouter.js
@@ -4,7 +4,6 @@ import { actionController } from '../controllers/actionController.js';
 export const actionRouter = new Router();
 
 actionRouter.get('/', actionController.getAll);
-actionRouter.get('/:id', actionController.getById);
 actionRouter.post('/', actionController.create);
 actionRouter.put('/', actionController.edit);
 actionRouter.delete('/', actionController.delete);
--- server/routes/categoryRouter.js
+++ server/routes/categoryRouter.js
@@ -3,7 +3,6 @@ import { categoryController } from '../controllers/categoryController.js';
 export const categoryRouter = new Router();
 
 categoryRouter.get('/', categoryController.getAll);
-categoryRouter.get('/:id', categoryController.getById);
 categoryRouter.post('/', categoryController.create);
 categoryRouter.put('/', categoryController.edit);
 categoryRouter.delete('/', categoryController.delete);
