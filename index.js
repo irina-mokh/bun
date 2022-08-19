d4e466bb4470cefb4baf12b35d60f2334d3f9aca
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-08-19 13:33:39 +0300

    fix: cors err

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ import bodyParser from 'body-parser';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: '*'
+}));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', router);
