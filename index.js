5cc5673a0251aea43da9f84ff2b61c78cc139b4e
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-08-19 13:49:06 +0300

    revert

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ import bodyParser from 'body-parser';
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use('/api', router);
