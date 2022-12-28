34d7dfc8fde01e6d48b71d6f8930666883639815
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-12-28 09:27:51 +0300

    test: revert for heroku server

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-import { app } from './api/index.js';
+import express from 'express';
 import { sequelize } from './db/index.js';
+import cors from 'cors';
+import { router } from './router/index.js';
+import { errorHandler } from './middleware/errorHandlingMiddleware.js';
 
 const PORT = process.env.PORT || 5002;
+export const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use('/', router);
+app.use(errorHandler);
+
+
+app.get('/',(req, res)=>{
+  res.status(200).json( { message: 'Working!'});
+})
 
 async function startApp() {
   try {
