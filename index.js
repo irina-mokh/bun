8387be244115d63444471d36ffbc259c63993cef
irina-mokh <75081244+irina-mokh@users.noreply.github.com>
2022-12-28 11:54:18 +0300

    refactor

--- index.js
+++ index.js
@@ -5,15 +5,13 @@ import { router } from './router/index.js';
 import { errorHandler } from './middleware/errorHandlingMiddleware.js';
 
 const PORT = process.env.PORT || 5002;
-export const app = express();
+const app = express();
 
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-
 app.use('/api', router);
 app.use(errorHandler);
-
 
 app.get('/',(req, res)=>{
   res.status(200).json( { message: 'Working!'});
